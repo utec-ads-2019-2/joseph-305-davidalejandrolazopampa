c4c0e5c76ac67d29a4e295579e72ee7ac1ce80df
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-10-14 19:28:27 -0500

    Iniciando

---
+++ cmake-build-debug/CMakeFiles/joseph_305_davidalejandrolazopampa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joseph_305_davidalejandrolazopampa.dir/main.cpp.obj"
+  "joseph_305_davidalejandrolazopampa.pdb"
+  "joseph_305_davidalejandrolazopampa.exe"
+  "joseph_305_davidalejandrolazopampa.exe.manifest"
+  "joseph_305_davidalejandrolazopampa.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joseph_305_davidalejandrolazopampa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by David Lazo on 14/10/2019.
+//
+#include <iostream>
+using  namespace std;
+int main(){
+    cout<<"Hola mundo";
+}
