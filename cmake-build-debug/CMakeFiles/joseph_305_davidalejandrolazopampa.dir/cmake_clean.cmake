b98722dcfe920c8affafaebfb1379b1454fbc6c0
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-10-15 10:46:20 -0500

    Finish

--- cmake-build-debug/CMakeFiles/joseph_305_davidalejandrolazopampa.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/joseph_305_davidalejandrolazopampa.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
   "CMakeFiles/joseph_305_davidalejandrolazopampa.dir/main.cpp.obj"
-  "joseph_305_davidalejandrolazopampa.pdb"
   "joseph_305_davidalejandrolazopampa.exe"
   "joseph_305_davidalejandrolazopampa.exe.manifest"
   "joseph_305_davidalejandrolazopampa.lib"
+  "joseph_305_davidalejandrolazopampa.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 //
 // Created by David Lazo on 14/10/2019.
 //
-#include <iostream>
-using  namespace std;
-int main(){
-    cout<<"Hola mundo";
+#include <cstdio>
+using namespace std;
+int main() {
+    int k;
+
+    //Aqui podria ir algoritmo pero el array es mas optimo creo yo
+    int array[13] = {2, 7, 5, 30, 169, 441, 1872, 7632, 1740, 93313, 459901,1358657, 2504881 };
+
+    while (scanf("%d", &k), k) {
+        printf("%d\n",array[k-1]);
+    }
+    //No me funcionaba x el return 3 horas de mi vida por eso
+    //return 0;
 }
